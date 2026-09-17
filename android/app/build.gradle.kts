9a5b438e2546bd164a670d58015202709144716f
Vladimir Matveenko <matvej83@gmail.com>
2026-09-17 18:17:55 +0300

    gradle upgraded to 9.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,10 +14,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.vladimir.dev.quiz_app"

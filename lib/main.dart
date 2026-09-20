872b6ad9ae47a27160373e931efa1fa1cccd66fa
Vladimir Matveenko <matvej83@gmail.com>
2026-09-20 18:38:26 +0300

    native splash added for mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quiz_app/app/constants/asset_paths.dart';
 
@@ -7,7 +9,10 @@ import 'app/di/injection.dart';
 import 'app/my_app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb) {
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  }
   await EasyLocalization.ensureInitialized();
   await configureDependencies();
   GoRouter.optionURLReflectsImperativeAPIs = true;

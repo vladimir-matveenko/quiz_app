c6460abf4df87e543b2b86ad1c7f4ee0485de022
vladimir-matveenko <55102679+vladimir-matveenko@users.noreply.github.com>
2026-09-20 15:40:53 +0000

    Deploying to gh-pages from @ vladimir-matveenko/quiz_app@a59ea513b4075f85f634ac92833a07be6b0a6edd 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "693632597" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2233926164" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
